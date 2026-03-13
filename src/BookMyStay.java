162c5c571f4a3ae576278c06c13785d175a740c9
Deva1302 <devanshi.srivastava1302@gmail.com>
2026-03-13 10:24:58 +0530

    Refactor: move BookMyStay.java into src folder

---
+++ src/BookMyStay.java
@@ -0,0 +1,40 @@
+/**
+ * =========================================================
+ * MAIN CLASS – BookMyStay
+ * =========================================================
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * Book My Stay Hotel Booking Management System.
+ *
+ * At this stage, the application:
+ * - Starts execution from the main() method
+ * - Displays a welcome message to the user
+ * - Confirms that the system has started successfully
+ *
+ * No business logic or data structures are implemented yet.
+ *
+ * @author Developer
+ * @version 1.0
+ */
+
+public class BookMyStay {
+
+    /**
+     * Application entry point
+     * Executed first when JVM starts the program
+     */
+    public static void main(String[] args) {
+
+        System.out.println("=======================================");
+        System.out.println("         Welcome to Book My Stay       ");
+        System.out.println("     Hotel Booking Management System   ");
+        System.out.println("               Version 1.0             ");
+        System.out.println("=======================================");
+
+        System.out.println("Application started successfully!");
+        System.out.println("System ready.");
+    }
+}
